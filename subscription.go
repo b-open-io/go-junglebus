41297b27c6c88bd34e04d890c831c181648cd206
Luke Rohenaz <rohenaz@gmail.com>
2025-02-03 16:41:51 -0500

    dont hand on centrifuge close

--- subscription.go
+++ subscription.go
@@ -31,6 +31,14 @@ type pubEvent struct {
 	Data    []byte
 }
 
+// 2025/02/03 21:38:01 [DEBUG] Unsubscribing from query:5af4235fe3e2a36965a46805a10dd48e0d659467c7f5df0a8c48ba5d32e406dd:mempool
+// 2025/02/03 21:38:01 [DEBUG] Unsubscribing from query:5af4235fe3e2a36965a46805a10dd48e0d659467c7f5df0a8c48ba5d32e406dd:control
+// 2025/02/03 21:38:01 [DEBUG] Unsubscribing from query:5af4235fe3e2a36965a46805a10dd48e0d659467c7f5df0a8c48ba5d32e406dd:882469:0
+// 2025/02/03 21:38:01 [DEBUG] Unsubscribed from subscriptions
+// 2025/02/03 21:38:01 [DEBUG] Closing pubChan
+// 2025/02/03 21:38:01 [DEBUG] Closed pubChan
+// 2025/02/03 21:38:01 Reconnecting to Junglebus
+
 func (s *Subscription) Unsubscribe() (err error) {
 
 	for _, sub := range s.subscriptions {
@@ -43,7 +51,7 @@ func (s *Subscription) Unsubscribe() (err error) {
 		close(s.pubChan)
 	}
 	log.Printf("[DEBUG] Closed pubChan")
-	s.centrifugeClient.Close()
+	go s.centrifugeClient.Close()
 	log.Printf("[DEBUG] Closed centrifugeClient")
 	s.closed = true
 	return err
